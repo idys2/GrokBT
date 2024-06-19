8e2dc0ba815d15b799a1894bcd075c7e0a404bb8
idys2 <irdnoun@gmail.com>
2024-06-18 22:05:25 -0400

    clean up

--- test/test_bencode.cpp
+++ test/test_bencode.cpp
@@ -1,11 +1,14 @@
-#include <assert.h>
 #include "metainfo.h"
 
+/**
+ * Tests bencode on a torrent (metainfo) file
+ */
 int main() {
 
 	// get the ip address of the tracker
-	std::string metainfo = Metainfo::read_metainfo(std::string(PROJECT_ROOT) + "/torrents/debian1.torrent");
-	std::string url = Metainfo::read_announce_url(metainfo);	
+	std::string path = std::string(PROJECT_ROOT) + "/torrents";
+	std::string metainfo = Metainfo::read_metainfo(path + "/debian1.torrent");
+	std::string url = Metainfo::read_announce_url(metainfo);
 	std::cout << "announce url: " << url << std::endl;
 
 	// read info dictionary
