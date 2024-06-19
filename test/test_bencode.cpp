f4ecdaa2ad9ed648fcf8a71d6f51ae3fffaeca16
idys2 <irdnoun@gmail.com>
2024-06-18 22:05:25 -0400

    clean up

--- test/test_bencode.cpp
+++ test/test_bencode.cpp
@@ -6,8 +6,9 @@
 int main() {
 
 	// get the ip address of the tracker
-	std::string metainfo = Metainfo::read_metainfo(std::string(PROJECT_ROOT) + "/torrents/debian1.torrent");
-	std::string url = Metainfo::read_announce_url(metainfo);	
+	std::string path = std::string(PROJECT_ROOT) + "/torrents";
+	std::string metainfo = Metainfo::read_metainfo(path + "/debian1.torrent");
+	std::string url = Metainfo::read_announce_url(metainfo);
 	std::cout << "announce url: " << url << std::endl;
 
 	// read info dictionary
